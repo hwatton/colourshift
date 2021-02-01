352eb6d448eea6a5aa4e384d9a48b222da2edd19
hwatton <66825106+hwatton@users.noreply.github.com>
2021-02-01 13:57:17 +0000

    adding levels

--- src/components/game/game.jsx
+++ src/components/game/game.jsx
@@ -1,33 +1,40 @@
-import Basics from "./levels/basics.jsx"
-import React, { Component } from "react"
+import Basics from "./levels/basics.jsx";
+import LevelTwo from "./levels/level2.jsx";
+import React, { Component } from "react";
 import styles from "./game.css";
 
+class Game extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      gameLevel: 0
+    };
 
-
-class Game extends Component{
-constructor(props) {
-    super(props)
-
-    this.state= {
-        gameLevel: 1,
-
-    }
-}
-/* ig gameLevel > 5 the rednder a grid based game.
+    this.changeLevel = this.changeLevel.bind(this);
+  }
+  /* ig gameLevel > 5 the rednder a grid based game.
 Make 5 levels without the grid.*/
 
-    render() {
-        return (
-            <div>
-                <div className="levelHolder">
-                <h2 className="level">Level: {this.state.gameLevel}</h2>
-                </div>
-            <Basics/>
-            </div>
-        )
-    }
-    
+  changeLevel() {
+    console.log("clicked");
+    const index = Math.floor(Math.random() * 2);
+    this.setState((state) => {
+      return { gameLevel: 1 };
+    });
+  }
+  render() {
+    let gameArray = [<Basics />, <LevelTwo />];
+    return (
+      <div>
+        <div className="levelHolder">
+          <h2 className="level">Level: {this.state.gameLevel + 1}</h2>
+          <button onClick={this.changeLevel}>Next Level</button>
+        </div>
+        <Basics />
+      </div>
+    );
+  }
 }
 
-export default Game
+export default Game;
---
+++ src/components/game/levels/level2.jsx
@@ -0,0 +1,135 @@
+import React, { Component } from "react";
+import * as D3 from "d3";
+import styles from "./basics.css";
+
+class LevelTwo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.level_one = this.level_one.bind(this);
+  }
+
+  componentDidMount() {
+    this.level_one();
+  }
+
+  componentDidUpdate() {
+    this.level_one();
+  }
+
+  level_one() {
+    //d3 stuff
+    const node = this.node;
+
+    let svgC = D3.select(node);
+    let tx = 350;
+    let ty = 250;
+    let tr = 35;
+
+    let movingGroup = svgC.append("g");
+    let circleGroup = svgC.append("g");
+
+    let cenCir = [{ x: tx, y: ty, r: tr, id: "centreCircle" }];
+
+    movingGroup
+      .selectAll("circle")
+      .data(cenCir)
+      .enter()
+      .append("circle")
+      .attr("id", (d) => {
+        return d.id;
+      })
+      .attr("cx", (d) => {
+        return d.x;
+      })
+      .attr("cy", (d) => {
+        return d.y;
+      })
+      .attr("r", (d) => {
+        return d.r;
+      })
+      .attr("fill", "rgb(0,255,0)");
+
+    let cDat = [{ x: 150, y: 250, r: 35, id: "mover" }];
+
+    let diffY, diffX;
+    const drag_handler = D3.drag()
+      .on("start", function (d, event) {
+        let thisX = D3.select(this).attr("cx");
+        let thisY = D3.select(this).attr("cy");
+        diffY = event.y - thisY;
+        diffX = event.x - thisX;
+      })
+      .on("drag", function (event, d) {
+        D3.select(this)
+          .attr("cx", (d.x = event.x - diffX))
+          .attr("cy", (d.y = event.y - diffY));
+      })
+      .on("end", function (event, d) {
+        let cX = D3.select(this).attr("cx");
+        let cY = D3.select(this).attr("cy");
+
+        if (cX > tx - tr && cX < tx + tr) {
+          //partytime
+          if (cY > ty - tr && cY < ty + tr) {
+            //double party
+
+            D3.select("#centreCircle")
+              .transition()
+              .duration(500)
+              .attr("fill", "rgb(255,255,0)");
+
+            D3.select(this)
+              .transition()
+              .duration(600)
+              .ease(D3.easeBounce)
+              .attr("cx", tx)
+              .attr("cy", ty)
+              .on("end", () => {
+                D3.select(this).remove();
+              });
+          }
+        }
+      });
+
+    let circles = circleGroup.selectAll("circle").data(cDat).enter();
+
+    circles
+      .append("circle")
+      .attr("id", (d) => {
+        return d.id;
+      })
+      .attr("cx", (d) => {
+        return d.x;
+      })
+      .attr("cy", (d) => {
+        return d.y;
+      })
+      .attr("r", (d) => {
+        return d.r;
+      })
+      .attr("fill", "rgb(255,0,0)")
+      .call(drag_handler);
+  }
+
+  render() {
+    return (
+      <div className="svgHolder">
+        <div className="instructionHolder">
+          <h4 className="instruction">Turn the green circle yellow</h4>
+        </div>
+        <svg
+          ref={(node) => (this.node = node)}
+          onClick={this.handleClick}
+          id="svgAnimation"
+          height="500"
+          width="500"
+          viewBox="0 0 500 500"
+          preserveAspectRatio="xMinYMin meet"
+        ></svg>
+      </div>
+    );
+  }
+}
+
+export default LevelTwo;
